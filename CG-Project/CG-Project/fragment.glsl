5939c465117884ce90142503afb3d82fcbca9849
정유리 <2024184029@tukorea.ac.kr>
2025-12-03 14:37:35 +0900

    vertex, fragment glsl 파일 수정 (텍스처 매핑용0

--- CG-Project/CG-Project/fragment.glsl
+++ CG-Project/CG-Project/fragment.glsl
@@ -1,23 +1,39 @@
 #version 330 core
 
-in vec3 Color;      // 버텍스 쉐이더에서 받은 원래 색상 (터널용)
-out vec4 FragColor; // 최종 출력 색상
+in vec3 outColor;
+in vec2 TexCoord;
 
-uniform float uBrightness;      // 밝기 조절 (터널 등)
-uniform vec3 uObjectColor;      // 물체 단색 (로봇용)
-uniform bool uUseObjectColor;   // true면 단색 사용, false면 원래 색 사용
+out vec4 FragColor;
+
+uniform vec3 uObjectColor;
+uniform int uUseObjectColor; // 1: 단색 모드, 0: 텍스처 모드
+uniform float uBrightness;   // 밝기 조절
+
+// 텍스처 관련
+uniform sampler2D outTexture;
+uniform int uUseTexture;     // 1: 텍스처 사용, 0: 미사용
 
 void main()
 {
     vec3 resultColor;
 
-    if(uUseObjectColor) {
-        // 로봇: C++에서 보내준 특정 색상(살색, 빨강 등) 사용
-        resultColor = uObjectColor; 
-    } else {
-        // 터널/배경: 원래 정점 색상에 밝기 적용
-        resultColor = Color * uBrightness;
+    if (uUseTexture == 1) {
+        // 텍스처 사용 시: 이미지 색상 가져오기
+        vec4 texColor = texture(outTexture, TexCoord);
+        resultColor = texColor.rgb;
     }
+    else if (uUseObjectColor == 1) {
+        // 단색 모드 시: 지정된 색상 사용
+        resultColor = uObjectColor;
+    }
+    else {
+        // 그 외: 정점 색상 사용 (그라데이션 등)
+        resultColor = outColor;
+    }
+
+    // 터널 밝기(uBrightness) 적용 (0보다 클 때만)
+    if (uBrightness > 0.0)
+        resultColor = resultColor * uBrightness;
 
     FragColor = vec4(resultColor, 1.0);
 }
--- CG-Project/CG-Project/vertex.glsl
+++ CG-Project/CG-Project/vertex.glsl
@@ -1,9 +1,11 @@
 #version 330 core
 
-layout (location = 0) in vec3 vPos;   // 위치 (0번 속성)
-layout (location = 1) in vec3 vColor; // 색상 (1번 속성)
+layout (location = 0) in vec3 vPos;       // 위치 (location 0)
+layout (location = 1) in vec3 vColor;     // 색상 (location 1)
+layout (location = 2) in vec2 vTexCoord;  // 텍스처 좌표 (location 2)
 
-out vec3 Color; // 프래그먼트 쉐이더로 넘겨줄 색상
+out vec3 outColor;
+out vec2 TexCoord;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,5 +14,6 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(vPos, 1.0);
-    Color = vColor;
+    outColor = vColor;
+    TexCoord = vTexCoord;
 }
